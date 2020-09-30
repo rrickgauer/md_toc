e2c32c9657c79c161a0d1b0499f7cf99b7eaceca
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-09-30 16:24:44 -0500

    updayte

--- dist/md_toc.py
+++ dist/md_toc.py
@@ -44,34 +44,32 @@ def encloseTocInDetails(toc, displayText = 'Click me to open'):
 ###############################################################################
 ###############################################################################
 
-if __name__ == "__main__":
-
-    # setup argument parser
-    parser = argparse.ArgumentParser(description="Generate a table of contents for your markdown file.")
-    parser.add_argument('-d', '--details', action="store_true", help="Enclose the toc in a <details> html tag")
-    parser.add_argument('-f', '--file', nargs=1, help="Name of the markdown file")
-    parser.add_argument('-c', '--copy', action="store_true", help="Copy output to clipboard")
-    args = parser.parse_args()
+# setup argument parser
+parser = argparse.ArgumentParser(description="Generate a table of contents for your markdown file.")
+parser.add_argument('-d', '--details', action="store_true", help="Enclose the toc in a <details> html tag")
+parser.add_argument('-f', '--file', nargs=1, help="Name of the markdown file")
+parser.add_argument('-c', '--copy', action="store_true", help="Copy output to clipboard")
+args = parser.parse_args()
 
 
-    # get the filename
-    fileName = getFileName()
+# get the filename
+fileName = getFileName()
 
-    # quit program if file does not exist
-    if not doesFileExist(fileName):
-        quitProgram('File does not exist')
+# quit program if file does not exist
+if not doesFileExist(fileName):
+    quitProgram('File does not exist')
 
-    # generate the toc
-    toc = getToc(fileName)
+# generate the toc
+toc = getToc(fileName)
 
-    # enclose the toc in a details tag if the user elected to
-    if args.details == True:
-        toc = encloseTocInDetails(toc)
+# enclose the toc in a details tag if the user elected to
+if args.details == True:
+    toc = encloseTocInDetails(toc)
 
-    # print output
-    print('\n' + toc)
+# print output
+print('\n' + toc)
 
-    # copy output to clipboard
-    if args.copy == True:
-       pyperclip.copy(toc)
-       print('\n\n\nCopied to clipboard') 
+# copy output to clipboard
+if args.copy == True:
+   pyperclip.copy(toc)
+   print('\n\n\nCopied to clipboard') 
